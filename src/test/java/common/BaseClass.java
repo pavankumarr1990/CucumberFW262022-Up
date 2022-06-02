968c59707185c1ddf6be9cdd8f5cc8792efea109
Pavan Kumar R <pavankumarr1990@gmail.com>
2022-06-02 20:43:14 +0530

    Created some files

--- src/test/java/common/BaseClass.java
+++ src/test/java/common/BaseClass.java
@@ -1,4 +1,33 @@
 package common;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
 public class BaseClass {
+    public static WebDriver driver;
+    @Before
+   public void initializeBrowser()
+   {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+
+
+   //       EventFiringWebDriver getDriver=new EventFiringWebDriver(driver);
+//       eventListener eL=new eventListener();
+//       getDriver.register(eL);
+    }
+
+
+    @After
+    public void tearUp()
+    {
+        driver.quit();
+    }
 }
--- src/test/resources/feature/prac.feature
+++ src/test/resources/feature/prac.feature
@@ -1,1 +1,5 @@
-ttt
+Feature: First Test Case
+
+  Scenario: Navigate to Google
+    Given User Navigates to Google website
+    Then Close the Browser
