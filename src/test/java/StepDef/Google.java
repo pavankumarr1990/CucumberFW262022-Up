43ca6c3b75ff409f59cdfd9879cd1a975d48fbaa
Pavan Kumar R <pavankumarr1990@gmail.com>
2022-06-03 10:38:51 +0530

    Created some files

--- src/test/java/StepDef/Google.java
+++ src/test/java/StepDef/Google.java
@@ -1,14 +1,19 @@
 package StepDef;
 
 import common.BaseClass;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebDriver;
 
 public class Google extends BaseClass {
 
-    BaseClass baseClass=new BaseClass();
+    BaseClass baseclass = new BaseClass();
+
     @Given("User Navigates to Google website")
     public void userNavigatesToGoogleWebsite() {
+        baseclass.initializeBrowser();
         driver.navigate().to("https://www.google.com/");
     }
 
--- src/test/java/common/BaseClass.java
+++ src/test/java/common/BaseClass.java
@@ -1,13 +1,24 @@
 package common;
 
-import io.cucumber.java.After;
+
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
+
 
 public class BaseClass {
     public static WebDriver driver;
+
+
+
+    public void initializeBrowser()
+    {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+    }
+
 }
